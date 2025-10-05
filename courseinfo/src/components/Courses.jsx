c37f4a824405246d3b5468b1720d72efdae43330
Alok Kumar Singh <kumarsinghalok28@gmail.com>
2025-10-05 23:37:18 +0530

    Done 2.1 to 2.5 ex of part2

---
+++ courseinfo/src/components/Courses.jsx
@@ -0,0 +1,30 @@
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = (props) => (
+  <div>
+    {props.parts.map(part => <Part key={part.id} part={part} />)}
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => {
+  const total = props.total.reduce((sum, acc) => sum + acc.exercises, 0);
+  return <p>Number of exercises {total}</p>
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total total={course.parts} />
+    </div>
+  )
+}
+
+export default Course;
